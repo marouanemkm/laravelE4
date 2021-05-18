2e3218ad847487a378b0b8a35de448f66ffd4c7f
Marouane <maroumkm@gmail.com>
2021-05-18 12:32:20 +0200

    App Title changed

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Laravel</title>
+        <title>M2L News</title>
 
         {{-- Import --}}
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
